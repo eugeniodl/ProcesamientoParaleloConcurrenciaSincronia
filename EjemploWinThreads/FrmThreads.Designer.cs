4f2262f6fce9d7e9ebefe49947546d71461f8ce0
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2025-03-25 11:50:56 -0600

    Se agregó EjemploWinThreads

---
+++ EjemploWinThreads/FrmThreads.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace EjemploWinThreads
+{
+    partial class FrmThreads
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnIniciar = new Button();
+            pb1 = new ProgressBar();
+            pb2 = new ProgressBar();
+            pb3 = new ProgressBar();
+            SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            btnIniciar.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnIniciar.Location = new Point(25, 59);
+            btnIniciar.Name = "btnIniciar";
+            btnIniciar.Size = new Size(117, 53);
+            btnIniciar.TabIndex = 0;
+            btnIniciar.Text = "Iniciar";
+            btnIniciar.UseVisualStyleBackColor = true;
+            btnIniciar.Click += btnIniciar_Click;
+            // 
+            // pb1
+            // 
+            pb1.Location = new Point(162, 31);
+            pb1.Name = "pb1";
+            pb1.Size = new Size(344, 21);
+            pb1.TabIndex = 1;
+            // 
+            // pb2
+            // 
+            pb2.Location = new Point(162, 73);
+            pb2.Name = "pb2";
+            pb2.Size = new Size(344, 21);
+            pb2.TabIndex = 2;
+            // 
+            // pb3
+            // 
+            pb3.Location = new Point(162, 118);
+            pb3.Name = "pb3";
+            pb3.Size = new Size(344, 21);
+            pb3.TabIndex = 3;
+            // 
+            // FrmThreads
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(531, 169);
+            Controls.Add(pb3);
+            Controls.Add(pb2);
+            Controls.Add(pb1);
+            Controls.Add(btnIniciar);
+            Name = "FrmThreads";
+            Text = "FrmThreads";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnIniciar;
+        private ProgressBar pb1;
+        private ProgressBar pb2;
+        private ProgressBar pb3;
+    }
+}
---
+++ EjemploWinThreads/FrmThreads.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploWinThreads
+{
+    public partial class FrmThreads : Form
+    {
+        private Thread hilo1, hilo2, hilo3;
+        delegate void delegado(int valor);
+        delegate void otroDelegado(ProgressBar progressBar, int valor);
+
+        public FrmThreads()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            hilo1 = new Thread(new ThreadStart(Proceso1));
+            hilo2 = new Thread(new ThreadStart(Proceso2));
+            hilo3 = new Thread(new ThreadStart(Proceso3));
+
+            hilo1.Start();
+            hilo2.Start();
+            hilo3.Start();
+        }
+
+        private void Proceso()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                otroDelegado MD = new otroDelegado(Actualizar);
+                Invoke(MD, new object[] { pb1, i });
+                Thread.Sleep(70);
+            }
+        }
+
+        private void Actualizar(ProgressBar pb, int valor)
+        {
+            pb.Value = valor;
+        }
+
+        private void Proceso1()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                delegado MD = new delegado(Actualizar1);
+                Invoke(MD, new object[] { i });
+                Thread.Sleep(70);
+            }
+        }
+
+        private void Actualizar1(int valor)
+        {
+            pb1.Value = valor;
+        }
+
+        private void Proceso2()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                delegado MD = new delegado(Actualizar2);
+                Invoke(MD, new object[] { i });
+                Thread.Sleep(40);
+            }
+        }
+
+        private void Actualizar2(int valor)
+        {
+            pb2.Value = valor;
+        }
+
+        private void Proceso3()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                delegado MD = new delegado(Actualizar3);
+                Invoke(MD, new object[] { i });
+                Thread.Sleep(100);
+            }
+        }
+
+        private void Actualizar3(int valor)
+        {
+            pb3.Value = valor;
+        }
+    }
+}
